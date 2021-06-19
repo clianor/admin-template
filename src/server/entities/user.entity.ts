ae1121f75dc2317fbefc22909311a689903b1e68
clianor <ejrgus94160@gmail.com>
2021-06-19 23:13:11 +0900

    password 암호화 추가

--- src/server/entities/user.entity.ts
+++ src/server/entities/user.entity.ts
@@ -1,7 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import CoreEntity from '@server/common/core.entity';
 import { IsEmail, IsString } from 'class-validator';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @ObjectType()
 @Entity()
@@ -15,4 +17,27 @@ export class UserEntity extends CoreEntity {
   @Field(() => String)
   @Column({ select: false })
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password) {
+      try {
+        this.password = await bcrypt.hash(this.password, 10);
+      } catch (error) {
+        console.log(error);
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkPassword(aPassword: string): Promise<boolean> {
+    try {
+      const ok = await bcrypt.compare(aPassword, this.password);
+      return ok;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
