a361b9253be903a00a001b0a505a63a40313ccd6
clianor <ejrgus94160@gmail.com>
2021-07-03 20:30:02 +0900

    :triangular_flag_on_post: seed module 작성
    - 기본 데이터를 추가하는 역할을 하는 seed module 추가

--- src/server/modules/common/common.module.ts
+++ src/server/modules/common/common.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import EnvConfig from '@server/configs/env.config';
 import GqlConfig from '@server/configs/graphql.config';
 import OrmConfig from '@server/configs/orm.config';
+import { SeedsModules } from '@server/modules/seeds/seeds.modules';
 
 @Module({
-  imports: [EnvConfig, OrmConfig, GqlConfig],
+  imports: [EnvConfig, OrmConfig, GqlConfig, SeedsModules],
 })
 export class CommonModule {}
